456cb5d4eb6c5618a127cdda6d17317a7d5c2026
yanchunhuo <DHxm10086>
2019-12-05 17:16:05 +0800

    update support windows encoding

--- init/java/java_maven_init.py
+++ init/java/java_maven_init.py
@@ -1,6 +1,7 @@
 from common.fileTool import FileTool
 import os
 import subprocess
+import platform
 
 def java_maven_init():
     print('开始java maven更新......')
@@ -9,5 +10,8 @@ def java_maven_init():
     print('删除旧的maven依赖包完成......')
     maven_update_command = 'mvn -U -f "' + os.path.join(os.getcwd(),'config/java/pom.xml"') + ' dependency:copy-dependencies -DoutputDirectory="' + os.path.join(os.getcwd(), 'common/java/lib/java/libs"')
     output = subprocess.check_output(maven_update_command, shell=True, timeout=3600)
-    print(output.decode('utf-8'))
+    if 'Windows' == platform.system():
+        print(output.decode('cp936'))
+    else:
+        print(output.decode('utf-8'))
     print('完成java maven更新......')
