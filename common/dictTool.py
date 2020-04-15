867451146373c1f26d4119b69bf2525b869e1faf
yanchunhuo <yanchunwei410@163.com>
2020-04-15 11:06:38 +0800

    add dict tool

---
+++ common/dictTool.py
@@ -0,0 +1,25 @@
+# @Author  : yanchunhuo
+# @Time    : 2020/4/15 11:01
+
+class DictTool:
+    @classmethod
+    def sorted_dict_in_list(cls, datas):
+        """
+        将数组内的字典按字母进行排序
+        :param datas:
+        :return:
+        """
+        new_datas = []
+        for data in datas:
+            new_data = {}
+            for key in sorted(data):
+                new_data.update({key: data[key]})
+            new_datas.append(new_data)
+        return new_datas
+
+    @classmethod
+    def sorted_dict(cls,data):
+        new_data = {}
+        for key in sorted(data):
+            new_data.update({key: data[key]})
+        return new_data
