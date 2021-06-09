08192b0ddc4262e783dbd70e001125fa17deed70
yanchunhuo <yanchunhuo@szy.cn>
2021-06-09 21:52:11 +0800

    update

--- common/monkey/monkey_client.py
+++ common/monkey/monkey_client.py
@@ -3,7 +3,7 @@
 # @author yanchunhuo
 # @description 
 # @created 2021-05-18T21:08:38.694Z+08:00
-# @last-modified 2021-06-08T20:23:10.370Z+08:00
+# @last-modified 2021-06-09T21:51:42.306Z+08:00
 # github https://github.com/yanchunhuo
 from common.dateTimeTool import DateTimeTool
 import platform
@@ -11,10 +11,12 @@ import subprocess
 
 class Monkey_Client:
     
-    def start_android_monkey(self,udid:str,package:str,log_file_path:str,throttle:int=100,event_times:int=100000,):
-        start_command = 'adb -s %s shell monkey -p %s --throttle %s --ignore-crashes --ignore-timeouts --ignore-security-exceptions --ignore-native-crashes --monitor-native-crashes -v -v -v %s >%s'\
-                %(udid,package,int(throttle),int(event_times),log_file_path)
-        sub_p = subprocess.Popen(start_command,shell=True)
+    def start_android_monkey(self, udid: str, package: str, std_log_file_path: str, err_log_file_path,
+                             throttle: int = 100,
+                             event_times: int = 100000, ):
+        start_command = 'adb -s %s shell monkey -p %s --throttle %s --ignore-crashes --ignore-timeouts --ignore-security-exceptions --ignore-native-crashes --monitor-native-crashes --pct-syskeys 0 -v -v -v %s 1 >%s   2>%s' \
+                        % (udid, package, int(throttle), int(event_times), std_log_file_path, err_log_file_path)
+        sub_p = subprocess.Popen(start_command, shell=True)
         return sub_p
     
     def stop_android_monkey(self,udid:str,sub_p=None):
@@ -26,15 +28,16 @@ class Monkey_Client:
             get_monkey_process_id_command='adb -s %s shell ps|findstr monkey'%udid
             try:
                 monkey_process_id=subprocess.check_output(get_monkey_process_id_command,shell=True)
+                monkey_process_id = monkey_process_id.decode('utf-8')
             except:
                 print('%s未查找到%s的monkey服务' %(DateTimeTool.getNowTime(),udid))
         else:
             get_monkey_process_id_command='adb -s %s shell ps|grep monkey'%udid
             try:
                 monkey_process_id=subprocess.check_output(get_monkey_process_id_command,shell=True)
+                monkey_process_id = monkey_process_id.decode('utf-8')
             except:
                 print('%s未查找到%s的monkey服务' %(DateTimeTool.getNowTime(),udid))
-        monkey_process_id=monkey_process_id.decode('utf-8')
         if monkey_process_id:
             get_lambda=lambda info:list(filter(None,info.split(' '))) if info else []
             monkey_process_id=get_lambda(monkey_process_id)
