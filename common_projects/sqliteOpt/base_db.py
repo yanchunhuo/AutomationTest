8489854ce934e0155b5e2c691390c42ad3f1f627
yanchunhuo <yanchunhuo@szy.cn>
2022-08-09 10:22:54 +0800

    Base_DB类新增支持根据单个字段排序

--- common_projects/sqliteOpt/base_db.py
+++ common_projects/sqliteOpt/base_db.py
@@ -3,9 +3,10 @@
 # @author yanchunhuo
 # @description 
 # @created 2022-08-04T09:52:09.525Z+08:00
-# @last-modified 2022-08-08T13:08:52.202Z+08:00
+# @last-modified 2022-08-09T10:22:25.053Z+08:00
 # github https://github.com/yanchunhuo
 
+from sqlalchemy import desc
 import copy
 
 class Base_DB:
@@ -13,20 +14,37 @@ class Base_DB:
         self.db_session=db_session
         self.model=model
     
-    def filter_object(self,obj:object):
+    def filter_object(self,obj:object,order_by_column:str=None,is_desc=False):
         attrs=obj.__dict__
         attrs.pop('_sa_instance_state')
-        result_object=self.db_session.query(self.model).filter_by(**attrs).first()
+        if order_by_column and is_desc:
+            result_object=self.db_session.query(self.model).filter_by(**attrs).order_by(desc(order_by_column)).first()
+        elif order_by_column and not is_desc:
+            result_object=self.db_session.query(self.model).filter_by(**attrs).order_by(order_by_column).first()
+        elif not order_by_column:
+            result_object=self.db_session.query(self.model).filter_by(**attrs).first()
         return result_object
 
-    def filter_objects(self,obj:object,page_index:int=None,page_size:int=None,):
+    def filter_objects(self,obj:object,page_index:int=None,page_size:int=None,order_by_column:str=None,is_desc=None):
         attrs=obj.__dict__
         attrs.pop('_sa_instance_state')
         if page_index and page_size:
-            result_objects=self.db_session.query(self.model).filter_by(**attrs).\
-                limit(page_size).offset((page_index-1)*page_size).all()
+            if order_by_column and is_desc:
+                result_objects=self.db_session.query(self.model).filter_by(**attrs).\
+                    order_by(desc(order_by_column)).limit(page_size).offset((page_index-1)*page_size).all()
+            elif order_by_column and not is_desc:
+                result_objects=self.db_session.query(self.model).filter_by(**attrs).\
+                    order_by(order_by_column).limit(page_size).offset((page_index-1)*page_size).all()
+            elif not order_by_column:
+                result_objects=self.db_session.query(self.model).filter_by(**attrs).\
+                    limit(page_size).offset((page_index-1)*page_size).all()
         else:
-            result_objects=self.db_session.query(self.model).filter_by(**attrs).all()
+            if order_by_column and is_desc:
+                result_objects=self.db_session.query(self.model).filter_by(**attrs).order_by(desc(order_by_column)).all()
+            elif order_by_column and not is_desc:
+                result_objects=self.db_session.query(self.model).filter_by(**attrs).order_by(order_by_column).all()
+            elif not order_by_column:
+                result_objects=self.db_session.query(self.model).filter_by(**attrs).all()
         return result_objects
     
     def add_object(self,obj:object):
