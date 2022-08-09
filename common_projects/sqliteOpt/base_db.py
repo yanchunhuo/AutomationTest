e15730077e6e921c673c08e44207063087bfdf42
yanchunhuo <yanchunhuo@szy.cn>
2022-08-09 14:07:00 +0800

    Base_DB类支持多字段排序

--- common_projects/sqliteOpt/base_db.py
+++ common_projects/sqliteOpt/base_db.py
@@ -3,10 +3,8 @@
 # @author yanchunhuo
 # @description 
 # @created 2022-08-04T09:52:09.525Z+08:00
-# @last-modified 2022-08-09T10:22:25.053Z+08:00
+# @last-modified 2022-08-09T14:06:38.203Z+08:00
 # github https://github.com/yanchunhuo
-
-from sqlalchemy import desc
 import copy
 
 class Base_DB:
@@ -14,36 +12,55 @@ class Base_DB:
         self.db_session=db_session
         self.model=model
     
-    def filter_object(self,obj:object,order_by_column:str=None,is_desc=False):
+    def filter_object(self,obj:object,order_by_columns:list=None):
+        """_summary_
+
+        Args:
+            obj (object): _description_
+            order_by_columns (list, optional): 
+                数据库字段名，如需降序使用desc方法，例：
+                from sqlalchemy import desc                
+                ['AGENT_ID',desc('ROLE_ID')]. Defaults to None.
+
+        Returns:
+            _type_: _description_
+        """
         attrs=obj.__dict__
         attrs.pop('_sa_instance_state')
-        if order_by_column and is_desc:
-            result_object=self.db_session.query(self.model).filter_by(**attrs).order_by(desc(order_by_column)).first()
-        elif order_by_column and not is_desc:
-            result_object=self.db_session.query(self.model).filter_by(**attrs).order_by(order_by_column).first()
-        elif not order_by_column:
+        if order_by_columns:
+            result_object=self.db_session.query(self.model).filter_by(**attrs).order_by(*order_by_columns).first()
+        else:
             result_object=self.db_session.query(self.model).filter_by(**attrs).first()
         return result_object
 
-    def filter_objects(self,obj:object,page_index:int=None,page_size:int=None,order_by_column:str=None,is_desc=None):
+    def filter_objects(self,obj:object,page_index:int=None,page_size:int=None,order_by_columns:list=None):
+        """_summary_
+
+        Args:
+            obj (object): _description_
+            page_index (int, optional): _description_. Defaults to None.
+            page_size (int, optional): _description_. Defaults to None.
+            order_by_columns (list, optional): 
+                数据库字段名，如需降序使用desc方法，例：
+                from sqlalchemy import desc                
+                ['AGENT_ID',desc('ROLE_ID')]. Defaults to None.
+
+        Returns:
+            _type_: _description_
+        """
         attrs=obj.__dict__
         attrs.pop('_sa_instance_state')
         if page_index and page_size:
-            if order_by_column and is_desc:
+            if order_by_columns:
                 result_objects=self.db_session.query(self.model).filter_by(**attrs).\
-                    order_by(desc(order_by_column)).limit(page_size).offset((page_index-1)*page_size).all()
-            elif order_by_column and not is_desc:
-                result_objects=self.db_session.query(self.model).filter_by(**attrs).\
-                    order_by(order_by_column).limit(page_size).offset((page_index-1)*page_size).all()
-            elif not order_by_column:
+                    order_by(*order_by_columns).limit(page_size).offset((page_index-1)*page_size).all()
+            else:
                 result_objects=self.db_session.query(self.model).filter_by(**attrs).\
                     limit(page_size).offset((page_index-1)*page_size).all()
         else:
-            if order_by_column and is_desc:
-                result_objects=self.db_session.query(self.model).filter_by(**attrs).order_by(desc(order_by_column)).all()
-            elif order_by_column and not is_desc:
-                result_objects=self.db_session.query(self.model).filter_by(**attrs).order_by(order_by_column).all()
-            elif not order_by_column:
+            if order_by_columns:
+                result_objects=self.db_session.query(self.model).filter_by(**attrs).order_by(*order_by_columns).all()
+            else:
                 result_objects=self.db_session.query(self.model).filter_by(**attrs).all()
         return result_objects
     
