afe37cfbb42a1d55d62f4226184a950bd079928a
yanchunhuo <yanchunhuo@szy.cn>
2022-11-17 15:48:59 +0800

    增强dubboclient返回值类型

--- common/dubboClient.py
+++ common/dubboClient.py
@@ -20,30 +20,35 @@ class DubboClient:
         self. _DubboClient=jpype.JClass("com.yanchunhuo.tools.DubboClient")
         self._dubboClient=self._DubboClient(registryAddresses,protocol,group)
 
-    def request(self,requestInterfaceClassName,requestMethod,params):
+    def request(self,requestInterfaceClassName,requestMethod,params,is_return_dict=True):
         """
         :param requestInterfaceClassName 请求接口类名,需完整包路径
         :param requestMethod 请求方法
         :param params 所有参数放在一个数组内，格式:
-                      无参数填写：[]
-                      参数值为null：填写null字符串即可，当前支持String、自定义对象类型
-                      一个基本数据类型参数(byte、short、int、long、double、float、boolean)：[{"type":"int","data":"value"}]
-                      一个基本数据类型数组参数：[{"type":"int[]","data":[{"type":"int","data":"value"},{"type":"int","data":"value"}]}]
-                      一个java.lang数据类型参数：[{"type":"java.lang.String","data":"1"}]
-                      一个java.lang数据类型数组参数：[{"type":"java.lang.String[]","data":[{"type":"java.lang.String","data":"value"},{"type":"java.lang.String","data":"value"}]}]
-                      一个集合类型参数：[{"type":"java.util.Set","data":[{"type":"java.lang.Long","data":"value"},{"type":"java.lang.Long","data":"value"}]}]
-                      一个MAP类型参数：[{"type":"java.util.Map","data":[[{"type":"key_type","data":"key_value"},{"type":"value_type","data":"value_value"}]]"}]
-                      一个枚举类类型：[{"type":"com.ztjy.authority.constants.SystemTypeEnum","data":"value"}]  其中value填写枚举类中的一个枚举值
-                      一个时间类型：[{"type":"java.util.Date","data":{"format":"yyyy-MM-dd HH:mm:ss","datetime":"value"}}]
+                      1、无参数填写：[]
+                      2、参数值为null：填写null字符串即可
+                      3、一个基本数据类型参数(byte、short、int、long、double、float、boolean)：[{"type":"int","data":"value"}]
+                      4、一个基本数据类型数组参数：[{"type":"int[]","data":[{"type":"int","data":"value"},{"type":"int","data":"value"}]}]
+                      5、一个java.lang数据类型参数：[{"type":"java.lang.String","data":"1"}]
+                      6、一个java.lang数据类型数组参数：[{"type":"java.lang.String[]","data":[{"type":"java.lang.String","data":"value"},{"type":"java.lang.String","data":"value"}]}]
+                      7、一个数组类型参数：[{"type":"java.util.List","data":[{"type":"java.lang.String","data":"value"},{"type":"java.lang.String","data":"value"}]}]
+                      8、一个集合类型参数：[{"type":"java.util.Set","data":[{"type":"java.lang.Long","data":"value"},{"type":"java.lang.Long","data":"value"}]}]
+                      9、一个MAP类型参数：[{"type":"java.util.Map","data":[[{"type":"java.lang.Long","data":"value"},{"type":"java.lang.Long","data":"value"}]]"}]
+                      10、一个枚举类类型参数：[{"type":"com.ztjy.authority.constants.SystemTypeEnum","data":"value"}]  其中value填写枚举类中的一个枚举值
+                      11、一个时间类型参数：[{"type":"java.util.Date","data":{"format":"yyyy-MM-dd HH:mm:ss","datetime":"value"}}]
                                   当前支持java.util.Date、java.time.LocalDate、java.sql.Timestamp、java.sql.Date
-                      一个自定义对象类型参数：[{"type":"com.company.xxxDTO","data":{"username":"username","age":344}}]
+                      12、一个自定义对象类型参数：[{"type":"com.company.xxxDTO","data":{"username":"username","age":344}}]
+        :param is_return_json 接口是否返回字典类型，否则返回字符串类型。当dubbo返回数据对象是HashMap时且key是一个对象，无法返回字典类型，故支持返回字符串类型
         :return
         """
         if not isinstance(params,str):
             params=ujson.dumps(params)
         result=self._dubboClient.request(requestInterfaceClassName,requestMethod,params)
         if result:
-            return ujson.loads(str(result))
+            if is_return_dict:
+                ujson.loads(str(result))
+            else:
+                return str(result)
 
     def object2Json(self,className):
         """
