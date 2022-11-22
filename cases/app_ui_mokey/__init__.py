6b7dd0aa4f31267e18cc94ef598444aa62a56626
yanchunhuo <yanchunhuo@szy.cn>
2022-11-22 15:39:56 +0800

    remove monkey

--- cases/app_ui_mokey/__init__.py
+++
@@ -1,8 +0,0 @@
-#
-# __init__.py
-# @author yanchunhuo
-# @description 
-# @created 2021-05-18T20:52:00.520Z+08:00
-# @last-modified 2021-05-20T18:03:37.907Z+08:00
-# github https://github.com/yanchunhuo
-
--- cases/app_ui_mokey/android/__init__.py
+++
@@ -1,8 +0,0 @@
-#
-# __init__.py
-# @author yanchunhuo
-# @description 
-# @created 2021-05-18T20:52:28.612Z+08:00
-# @last-modified 2021-05-20T18:03:35.431Z+08:00
-# github https://github.com/yanchunhuo
-
